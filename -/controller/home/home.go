fe98f949e277b2d1eb373249de6ec4b800eb4ad3
kevinrizkhy <kevin.rizkhy85@gmail.com>
2018-06-21 13:12:47 +0700

    bypass db

--- -/controller/home/home.go
+++ -/controller/home/home.go
@@ -1,10 +1,10 @@
 package home
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/wellcode/LCWB/-/config"
-	session "github.com/wellcode/LCWB/-/model/session"
-	user "github.com/wellcode/LCWB/-/model/user"
+	//session "github.com/wellcode/LCWB/-/model/session"
+	//user "github.com/wellcode/LCWB/-/model/user"
 	"html/template"
 	"net/http"
 )
@@ -12,20 +12,21 @@ import (
 var t *template.Template
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	session_val := session.CheckSession(r)
-	if session_val != nil {
-		t, _ = template.ParseFiles(
-			"-/view/home.html",
-			"-/view/partial/table.html",
-		)
-		sesion_str := fmt.Sprint(session_val)
-		data := user.GetUserData(sesion_str)
-		data["BaseURL"] = config.Base_URL
-		data["weblist"] = template.JS(user.GetUserWebList(sesion_str))
-		t.ExecuteTemplate(w, "layout", data)
-	} else {
-		http.Redirect(w, r, "/sign", 302)
-	}
+	//r.ParseForm()
+	//session_val := session.CheckSession(r)
+	//if session_val != nil {
+	t, _ = template.ParseFiles(
+		"-/view/home.html",
+		"-/view/partial/table.html",
+	)
+	//sesion_str := fmt.Sprint(session_val)
+	//data := user.GetUserData(sesion_str)
+	data := make(map[string]interface{})
+	data["BaseURL"] = config.Base_URL
+	//data["weblist"] = template.JS(user.GetUserWebList(sesion_str))
+	t.ExecuteTemplate(w, "layout", data)
+	//} else {
+	//	http.Redirect(w, r, "/sign", 302)
+	//}
 
 }
--- -/model/sign/sign_in.go
+++ -/model/sign/sign_in.go
@@ -10,10 +10,6 @@ import (
 func Login(email string, password string) (bool, string) {
 	rows, err := database.ExecuteQuery("SELECT id from t_user where email=$1 and password=$2", email, password)
 	if len(rows) > 0 && err == nil {
-		//pass := rows[0]["password"]
-		//errHash := bcrypt.CompareHashAndPassword([]byte(pass), []byte(password))
-		//errHash := nil
-		//if errHash == nil {
 		sessionToken := utility.GenerateToken(email)
 		_, err := database.ExecuteQuery("update t_user set session_token=$1 where email=$2", sessionToken, email)
 		if err == nil {
@@ -22,9 +18,6 @@ func Login(email string, password string) (bool, string) {
 			fmt.Println("Login - 1", err)
 			return false, ""
 		}
-		//} else {
-		//	return false, ""
-		//}
 	}
 	return false, ""
 }
