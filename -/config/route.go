a4a421fb53499b5188a3e58342791d1b9096a789
kevinrizkhy <kevin.rizkhy85@gmail.com>
2018-06-21 13:15:58 +0700

    bypass

--- -/config/route.go
+++ -/config/route.go
@@ -1,9 +1,9 @@
 package config
 
 const (
-	Base_Folder = "github.com/kevinrizkhy/Internal/"
-	Base_Port   = ":6661"
-	Base_URL    = "/" //http://127.0.0.1" + Base_Port + "/"
+	//Base_Folder = "github.com/kevinrizkhy/Internal/"
+	Base_Port = ":6661"
+	Base_URL  = "/" //http://127.0.0.1" + Base_Port + "/"
 
 	WK_PATH = "F:/GoFILE/src/github.com/SebastiaanKlippert/wkhtmltopdf/bin/wkhtmltopdf.exe"
 )
