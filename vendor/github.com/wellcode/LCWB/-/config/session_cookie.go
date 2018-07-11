15129484aacf8aa17eda20297de3c3e841dd5c3b
kevinrizkhy <kevin.rizkhy85@gmail.com>
2018-07-11 17:55:50 +0700

    user ui

---
+++ vendor/github.com/wellcode/LCWB/-/config/session_cookie.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	COOKIE_NAME_TOKEN  = "kXpIV89wIQUxA86p2uWIAuOfflmGNZyYP"
+	SESSION_TOKEN_TIME = 3600 * 24
+)
---
+++ vendor/github.com/wellcode/LCWB/-/controller/preview/preview.go
@@ -0,0 +1,40 @@
+package preview
+
+import (
+	//"fmt"
+	session "github.com/wellcode/LCWB/-/model/session"
+	//user "github.com/wellcode/LCWB/-/model/user"
+	file "github.com/wellcode/LCWB/-/model/file"
+	html "github.com/wellcode/LCWB/-/model/html"
+	"html/template"
+	"net/http"
+)
+
+var t *template.Template
+
+func GenerateFile(web_id, web_name string) {
+	path := "-/file/" + web_name
+	file.CreatePreviewFolder(path)
+	index, css, js := html.GetUserWebPartCode(web_id)
+	file.CreateFile(path + "/index.html")
+	file.CreateFile(path + "/css/style.css")
+	file.CreateFile(path + "/js/script.js")
+
+	//write to file
+	file.WriteHTML(path+"/index.html", index)
+	file.WriteHTML(path+"/css/style.css", css)
+	file.WriteHTML(path+"/js/script.js", js)
+}
+
+func Preview(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	session_val := session.CheckSession(r)
+	if session_val != nil {
+		web_id := r.FormValue("web_id")
+		web_name := r.FormValue("web_name")
+		GenerateFile(web_id, web_name)
+	} else {
+		http.Redirect(w, r, "/sign", 302)
+	}
+
+}
